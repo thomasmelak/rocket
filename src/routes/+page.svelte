a1916f514cbd126c5e474528da9d158e59bcaafd
Thomas Melak <me@thomasmelak.io>
2025-07-27 01:30:33 +0300

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  // Ensure the path is correct and the file exists
+
+  import Scene from '$lib/Scene.svelte';
+  import { Canvas } from '@threlte/core';
+</script>
+
+<Canvas>
+  <Scene />
+</Canvas>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the 
+    documentation</p>
